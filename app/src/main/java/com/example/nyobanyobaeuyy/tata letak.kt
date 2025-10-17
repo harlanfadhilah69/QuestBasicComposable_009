6d774edbddfb37b26d2d739711fd0fbfc107bbbb
harlanfadhilah69 <harlancoegg@gmail.com>
2025-10-17 16:35:55 +0700

    memmbuat ukuran image dan warna text

--- app/src/main/java/com/example/nyobanyobaeuyy/tata letak.kt
+++ app/src/main/java/com/example/nyobanyobaeuyy/tata letak.kt
@@ -1,5 +1,6 @@
 package com.example.nyobanyobaeuyy
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -14,9 +15,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun TataletakColumn(modifier: Modifier) {
@@ -107,7 +110,12 @@ fun  TataletakBoxColumnRow(modifier: Modifier) {
                 .background(color = Color.Cyan),
             contentAlignment = Alignment.Center
         ){
-
+            Image(painter = gambar,
+                contentDescription = null,
+                contentScale = ContentScale.Fit)
+            Text(text = "My Music",
+                fontSize = 50.sp,
+                color = Color.Red)
         }
     }
 }
